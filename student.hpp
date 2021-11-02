5cbddcb913846871425ba2bc492e2077662b7ed8
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-11-02 13:55:28 -0400

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -12,17 +12,17 @@ string exerciseOne (/*you need a word and then a number in here*/)
 	return "0";//TODO: Fix this
 }
 
-string exerciseTwo (/*you need a number*/)
+string exerciseTwo (int number)
 {
 	return "0";//TODO: Fix this
 }
 
-string exerciseThree (/*you need a number*/)
+string exerciseThree (int number)
 {
 	return "0";//TODO: Fix this
 }
 
-int exerciseFour (/*you need a number*/)
+int exerciseFour (int number)
 {
 	return 0;//TODO: Fix this
 }
